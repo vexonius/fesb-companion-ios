b22c8a2ed7f26904584ea99c471adbe9d93d6c1a
vexonius <tino@etino.dev>
2024-11-14 16:40:11 +0100

    Added Strings catalog support

---
+++ FESBCompanion/Sources/LocalizableStrings.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+extension String {
+
+    static let home = String(localized: "home")
+    static let settings = String(localized: "settings")
+    static let attendance = String(localized: "attendance")
+    static let timetable = String(localized: "timetable")
+    static let studomat = String(localized: "studomat")
+    static let xcard = String(localized: "xcard")
+
+
+}
