c9aecfe4dcb14a66260c2c5f19a7cf6b62df8daa
vexonius <tino@etino.dev>
2024-12-02 23:58:36 +0100

    Removed unused image constants

--- FESBCompanion/Sources/Images.swift
+++ FESBCompanion/Sources/Images.swift
@@ -5,10 +5,7 @@ extension Image {
     static let card = Image("card")
     static let pieChart = Image("pie-chart")
     static let house = Image("home")
-    static let studentCard = Image("student-card")
     static let calendar = Image("calendar")
     static let hexagon = Image("hexagon")
-    static let lamp = Image("lamp")
-    static let scroll = Image("scroll")
 
 }
