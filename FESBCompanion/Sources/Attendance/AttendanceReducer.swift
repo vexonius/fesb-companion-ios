19269ce7ecdf6f59580dcd5889d9aea47c837a10
vexonius <tino@etino.dev>
2024-12-06 21:28:45 +0100

    Added filtering for Attendance items on semester selection

--- FESBCompanion/Sources/Attendance/AttendanceReducer.swift
+++ FESBCompanion/Sources/Attendance/AttendanceReducer.swift
@@ -5,19 +5,59 @@ struct AttendanceReducer {
 
     @ObservableState
     struct State: Equatable {
+
+        var selectedSemester: Semester?
+
+        var attendanceItems: [AttendanceModel] {
+            guard let semester = selectedSemester else { return attendance }
+
+            return attendance.filter { $0.semester == semester }
+        }
+
+        private var attendance: [AttendanceModel] = [
+            AttendanceModel(
+                class: "Math",
+                semester: .winter,
+                items: [
+                    AttendanceItem(type: "Class", attended: 8, absent: 2, required: 8, semester: 1, total: 10),
+                    AttendanceItem(type: "Exam", attended: 8, absent: 2, required: 8, semester: 1, total: 10),
+                    AttendanceItem(type: "Auditory class", attended: 8, absent: 2, required: 8, semester: 1, total: 10)
+                ]
+            ),
+            AttendanceModel(
+                class: "Physics",
+                semester: .summer,
+                items: [
+                    AttendanceItem(type: "Class", attended: 8, absent: 2, required: 8, semester: 1, total: 10),
+                    AttendanceItem(type: "Exam", attended: 8, absent: 2, required: 8, semester: 1, total: 10),
+                    AttendanceItem(type: "Auditory class", attended: 8, absent: 2, required: 8, semester: 1, total: 10)
+                ]
+            )
+        ]
+
     }
 
     enum Action: Equatable, ViewAction {
 
         case view(View)
 
-        enum View: Equatable {}
+        enum View: Equatable {
+
+            case fetch
+            case refresh
+            case select(semester: Semester)
+
+        }
 
     }
 
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
+            case .view(.select(let semester)):
+                state.selectedSemester = semester == state.selectedSemester ? nil : semester
+
+                return .none
             default:
                 return .none
             }
--- FESBCompanion/Sources/Attendance/AttendanceView.swift
+++ FESBCompanion/Sources/Attendance/AttendanceView.swift
@@ -7,10 +7,84 @@ struct AttendanceView: View {
     @Bindable var store: StoreOf<AttendanceReducer>
 
     var body: some View {
-        ScrollView {
+        NavigationView {
+            ScrollView {
+                HStack(spacing: Padding.base.rawValue) {
+                    ForEach(Semester.allCases) { semester in
+                        Chip(text: semester.title, isSelected: semester == store.selectedSemester)
+                            .onTapGesture { send(.select(semester: semester)) }
+                    }
+                }
+                .padding(.top, .base)
+                .padding(.bottom, .medium)
+                .padding(.horizontal, .medium)
+                .maxWidth(alignment: .leading)
 
+                LazyVStack(spacing: Padding.medium.value) {
+                    ForEach(store.attendanceItems) { model in
+                        attendanceCard(model: model)
+                    }
+                }
+                .padding(.horizontal, .medium)
+            }
+            .refreshable {
+                send(.refresh)
+                try? await Task.sleep(nanoseconds: 10 * 1_000_000_000)
+            }
+            .maxSize()
+            .background(Color.surface.ignoresSafeArea())
+            .navigationTitle(String.attendance)
+            .onAppear { send(.fetch) }
         }
-        .background(Color.surface.ignoresSafeArea())
+    }
+
+    private func attendanceCard(model: AttendanceModel) -> some View {
+        VStack(alignment: .leading, spacing: Padding.small.value) {
+            Text(model.class)
+                .font(Font.headline)
+                .foregroundStyle(Color.text)
+                .padding(.bottom, .base)
+
+            ForEach(model.items) { item in
+                attendanceItem(model: item)
+            }
+        }
+        .padding(.medium)
+        .maxWidth(alignment: .leading)
+        .background(Color.container)
+        .clipShape(RoundedRectangle(cornerRadius: 30))
+    }
+
+    public func attendanceItem(model: AttendanceItem) -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(model.type)
+                .font(Font.body)
+                .foregroundStyle(Color.text)
+                .padding(.bottom, .base)
+
+            HStack(spacing: 6) {
+                progressDot(color: .green)
+                progressDot(color: .green)
+                progressDot(color: .green)
+                progressDot(color: .green)
+                progressDot(color: .green)
+                progressDot(color: .surface)
+                progressDot(color: .surface)
+                progressDot(color: .surface)
+                progressDot(color: .red)
+            }
+
+            Text(String(format: .attendanceProgressFormat, model.attended, model.required))
+                .font(Font.body)
+                .foregroundStyle(Color.text)
+                .padding(.bottom, .base)
+            }
+        }
+
+    private func progressDot(color: Color, radius: CGFloat = 10) -> some View {
+        Circle()
+            .fill(color)
+            .frame(width: 2 * radius, height: 2 * radius)
     }
 
 }
