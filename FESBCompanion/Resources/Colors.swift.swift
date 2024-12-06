9b6aeb831d2505cdd6de45a7df225b60ed6ede8a
vexonius <tino@etino.dev>
2024-12-06 13:04:50 +0100

    Added color resources

---
+++ FESBCompanion/Resources/Colors.swift.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+extension Color {
+
+    static let surface = Color("SurfaceColor")
+    static let container = Color("ContainerColor")
+    static let text = Color("TextColor")
+    static let accentBlue = Color("BlueAccentColor")
+    static let accentGreen = Color("GreenAccentColor")
+    static let accentRed = Color("RedAccentColor")
+
+}
