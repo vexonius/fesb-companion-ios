f2fb5500f1698a1da8977699818755075a4b1857
vexonius <tino@etino.dev>
2024-12-12 14:27:43 +0100

    Set up appearance for the whole app

---
+++ FESBCompanion/Sources/AppDelegate.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        setAppearance()
+
+        return true
+    }
+
+    func setAppearance() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithDefaultBackground()
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+    }
+
+}
--- FESBCompanion/Sources/FESBCompanionApp.swift
+++ FESBCompanion/Sources/FESBCompanionApp.swift
@@ -4,6 +4,8 @@ import ComposableArchitecture
 @main
 struct FESBCompanionApp: App {
 
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+
     @Bindable private var store: StoreOf<RootReducer> = Store(
         initialState: RootReducer.State(),
         reducer: RootReducer.init)
